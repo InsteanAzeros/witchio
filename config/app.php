efce273d25a3b9ca728f543e3f7e7ac8e95fca2f
Simon <fctinstean@gmail.com>
2020-09-01 12:07:10 +0200

    blabla

--- config/app.php
+++ config/app.php
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', base64_decode('dnh7HTQOAzMbh0q4+sBd4jxRVcKzuT85WTojTiYQCro=')),
 
     'cipher' => 'AES-256-CBC',
 
